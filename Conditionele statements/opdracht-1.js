f268a968098f3bcf2e978d2c0ea1daf44742397c
Maarten Paulissen <maarten.paulissen99@gmail.com>
2024-10-24 19:06:12 +0200

    opdracht 1 werkende

---
+++ Conditionele statements/opdracht-1.js
@@ -0,0 +1,13 @@
+const docent = "Jordy"
+
+switch (docent) {
+case "Jordy"
+console.log("Hallo Jordy!")
+break;
+case "Mark"
+console.log("Hallo Mark!")
+case " "
+console.log("Hallo vreemdeling!")
+}
+
+
